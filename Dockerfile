2769c6d7f77b8b501e4512eb5bf8ae3ee63535b9
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-09-24 20:18:40 +0200

    Updating dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
 FROM blockchainit/rust-wasm-env:latest
-COPY ./ ./
+ARG WORKSPACE_DIR=/usr/src/app
+WORKDIR ${WORKSPACE_DIR}
+ARG USER
+ARG PASS
+ARG EMAIL
+ENV USER=${USER}
+ENV PASS=${PASS}
+ENV EMAIL=${EMAIL}
+COPY . .
 RUN npm run build 
+RUN sh scripts/publish.sh
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 set -e
 export TAG="latest"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build --file Dockerfile --tag blockchainit/zokrates-js-node:$TAG .
+docker build --build-arg USER=$USER --build-arg PASS=$PASS --build-arg EMAIL=$EMAIL --file Dockerfile --tag blockchainit/zokrates-js-node:$TAG .
 docker push blockchainit/zokrates-js-node:$TAG
