393331b6ae095c7c374253707fd4edcd1c591624
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-09-24 20:51:14 +0200

    Updating

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,5 @@ npm install -g npm-cli-login
 NPM_USER=$USER NPM_PASS=$PASS NPM_EMAIL=$EMAIL npm-cli-login
 
 npm install -g publish
-npm version patch
+npm version patch --force --commit-hooks=false --no-git-tag-version
 npm publish
