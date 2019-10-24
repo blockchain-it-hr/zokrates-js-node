8dd73ab516a3da8862569b92ea9c01dc4369f977
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-10-24 15:11:13 +0200

    Delete build_docker.sh

--- scripts/build_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f Dockerfile -t blockchainit/zokrates-js-node:latest .
-docker push blockchainit/zokrates-js-node:latest
